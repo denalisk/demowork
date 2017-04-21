49666c7dad0df724061e645f50128c3b6f851d6e
Sam Kirsch <samdkirsch@gmail.com>
2017-04-20 17:31:08 -0700

    create template files

---
+++ src/GummyBear/Controllers/GummyBearController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummyBear.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummyBear.Controllers
+{
+    public class GummyBearController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/GummyBear/Models/GummyBearDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummyBear.Models;
+
+namespace GummyBear.Models
+{
+    public class GummyBearDbContext : DbContext
+    {
+        public GummyBearDbContext()
+        {
+
+        }
+
+        public DbSet<GummyBear> GummyBear { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummyBearDb;integrated security=True");
+        }
+
+        public GummyBearDbContext(DbContextOptions<GummyBearDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
